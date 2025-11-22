a4f639ebaefbdbf869ef8abda080b4657e1f77d7
DepaduaChristine <tinadepadua19@gmail.com>
2025-11-22 12:09:10 +0800

    login

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config.js';
+
 import bookRoutes from "./routers/bookRoutes.js";
 import studentRoutes from "./routers/StudentRoutes.js";
+import userRoutes from "./routers/UserRoutes.js";
 
 //create expres app
 const app = express();
+
 //enable cors tofrontend
 let corsOptions = {
     origin: process.env.ORIGIN
@@ -15,20 +18,23 @@ let corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
-//this used to log the reqeust on the console
+//this used to log the request on the console
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 
-try{
-    app.listen(process.env.PORT || 3000, () => {
-        console.log(`listening to port ${process.env.PORT || port}...`);
-    });
-}catch(e){
-    console.log(e);
-}
-
+// --- DITO DAPAT ANG ROUTES (Bago mag-listen) ---
 app.use('/book', bookRoutes);
 app.use('/student', studentRoutes);
+app.use('/user', userRoutes); 
+// ----------------------------------------------
 
+try {
+    const port = process.env.PORT || 3000; // Define port variable here for safety
+    app.listen(port, () => {
+        console.log(`listening to port ${port}...`);
+    });
+} catch(e) {
+    console.log(e);
+}
